b13e59b95bb9f36a7c5f2fee42b44fed7ca3e3c1
Sean Hill <TonkaTruck@Seans-MacBook-Air.local>
2015-08-11 11:42:44 -0700

    added Post controller and laid the groundwork for profile pictures

--- app/controllers/posts_controller.rb
+++ app/controllers/posts_controller.rb
@@ -1,7 +1,58 @@
 class PostsController < ApplicationController
+  def index
+    @posts = Post.all
+    render :index
+  end
+
+  # Method to create a new post
+  def create
+    if current_user
+      post = current_user.posts.create(post_params)
+      if post.save 
+        # if the post saves redirect to the post path with the new post
+        redirect_to post_path(post)
+      else 
+        flash[:error] = "Something went wrong, please try again"
+      end
+    else
+      redirect_to login_path
+      flash[:error] = "Please log in to post"
+    end
+  end
+
   def new
+    @post = Post.new
+    render :new
   end
 
   def show
+    id = params[:id]
+    post = User.post.find(id)
+    render :show
   end
+
+  def delete
+    id = params[:id]
+    post = User.post.find(id)
+    post.destroy
+    redirect_to profile_path
+  end 
+
+  def edit
+    id = params[:id]
+    post = User.post.find(id)
+    render :edit
+  end
+
+  def update
+    id = params[:id]
+    post = User.post.find(id)
+    post.update_attributes(post_params)
+    redirect_to user_post_path
+  end
+
+  private
+    def post_params
+      params.require(:post).permit(:title, :description, :avatar, :city)
+
 end
