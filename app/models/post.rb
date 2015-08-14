fb71c9812ab759c8c0639d586a8cf0a9d2ff0d1a
Alison <alisontong@hotmail.com>
2015-08-13 18:02:12 -0700

    undo some errors

--- app/models/post.rb
+++ app/models/post.rb
@@ -3,6 +3,7 @@ class Post < ActiveRecord::Base
   belongs_to :user
 
   has_many :comments, dependent: :destroy
+  
 
   validates :title, length: { minimum: 2, maximum: 255 }, presence: true
   validates :description, length: { minimum: 3, maximum: 255 }, presence: true
